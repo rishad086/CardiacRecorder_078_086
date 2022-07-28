eb6ddf911f63af59a3a8335b728c7627afb1309f
rishad <rishad.cse18.kuet@gmail.com>
2022-07-28 14:13:06 +0600

    first commit

---
+++ app/src/main/java/com/example/cardiacrec_078_086/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.cardiacrec_078_086;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ///tttt
+    }
+}
